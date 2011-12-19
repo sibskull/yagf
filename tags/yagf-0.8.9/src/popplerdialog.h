fb19b3a970cb3dc4288512dde8ff4584bae950f0
andrei.borovsky@gmail.com <andrei.borovsky@gmail.com@bb55e26e-657b-11de-982f-55b50260c994>
2011-12-19 04:54:20 +0000

    git-svn-id: https://yagf.googlecode.com/svn@215 bb55e26e-657b-11de-982f-55b50260c994

---
+++ tags/yagf-0.8.9/src/popplerdialog.h
@@ -0,0 +1,29 @@
+#ifndef POPPLERDIALOG_H
+#define POPPLERDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class PopplerDialog;
+}
+
+class PopplerDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PopplerDialog(QWidget *parent = 0);
+    ~PopplerDialog();
+    QString getPDFFile();
+    QString getStartPage();
+    QString getStopPage();
+private slots:
+    void on_checkBox_toggled(bool checked);
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::PopplerDialog *ui;
+};
+
+#endif // POPPLERDIALOG_H

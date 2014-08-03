28d4d57aaa2dbcd3dc80451292ca11ea0fd10b87
Andrei Borovskyi <anb@symmetrica.net>
2014-08-03 06:38:19 +0400

    b5

--- tags/yagf-0.9.3/src/scanimage.cpp
+++
@@ -1,6 +0,0 @@
-#include "scanimage.h"
-
-ScanImage::ScanImage(QObject *parent) :
-    QObject(parent)
-{
-}
--- trunk/src/translations/ts_update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-lupdate -noobsolete ../mainform.cpp ../mainform.ui ../popplerdialog.ui ../configdialog.ui ../langselectdialog.ui ../main.cpp ../spellchecker.cpp ../sidebar.cpp ../settings.cpp ../configdialog.cpp ../langselectdialog.cpp -ts yagf_ru.ts yagf_fr.ts
